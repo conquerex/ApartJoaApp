621022960d21426c60d7e0322f8a65157bf2dccd
Jongkook <conquerex@gmail.com>
2023-04-29 01:06:05 +0900

    멀티 터치 막기

---
+++ lib/src/only_one_pointer_recognizer_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class OnlyOnePointerRecognizer extends OneSequenceGestureRecognizer {
+  int _p = 0;
+
+  @override
+  void addPointer(PointerDownEvent event) {
+    startTrackingPointer(event.pointer);
+
+    if (_p == 0) {
+      resolve(GestureDisposition.rejected);
+      _p = event.pointer;
+    } else {
+      resolve(GestureDisposition.accepted);
+    }
+  }
+
+  @override
+  String get debugDescription => 'only one pointer recognizer';
+
+  @override
+  void didStopTrackingLastPointer(int pointer) {}
+
+  @override
+  void handleEvent(PointerEvent event) {
+    if (!event.down && event.pointer == _p) {
+      _p = 0;
+    }
+  }
+}
+
+class OnlyOnePointerRecognizerWidget extends StatelessWidget {
+  const OnlyOnePointerRecognizerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawGestureDetector(gestures: <Type, GestureRecognizerFactory>{
+      OnlyOnePointerRecognizer:
+          GestureRecognizerFactoryWithHandlers<OnlyOnePointerRecognizer>(
+              () => OnlyOnePointerRecognizer(),
+              (OnlyOnePointerRecognizer instance) {})
+    });
+  }
+}
--- lib/src/web_view_stack.dart
+++ lib/src/web_view_stack.dart
@@ -2,6 +2,8 @@ import 'package:apart_joa_app/src/webview_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'only_one_pointer_recognizer_widget.dart';
+
 class WebViewMain extends StatefulWidget {
   const WebViewMain({Key? key}) : super(key: key);
 
@@ -12,6 +14,13 @@ class WebViewMain extends StatefulWidget {
 class _WebViewMainState extends State<WebViewMain> {
   final controller = WebviewMainController.to.getController();
 
+  @override
+  void initState() {
+    super.initState();
+    // controller.loadRequest(Uri.parse('http://43.200.37.141/user/login/'));
+    controller.enableZoom(false);
+  }
+
   //home으로 가는 버튼 함수
   void goHome() {
     controller.loadRequest(Uri.parse('http://43.200.37.141/user/login/'));
@@ -20,9 +29,8 @@ class _WebViewMainState extends State<WebViewMain> {
   //앱 나가기 전 dialog
   Future<bool> showExitPopup() async {
     return await showDialog(
-      context: context,
-      builder: (context) =>
-          AlertDialog(
+          context: context,
+          builder: (context) => AlertDialog(
             title: const Text('Exit App'),
             content: const Text('앱을 종료하시겠습니까?'),
             actions: [
@@ -37,12 +45,14 @@ class _WebViewMainState extends State<WebViewMain> {
               ),
             ],
           ),
-    ) ?? false;
+        ) ??
+        false;
   }
 
   //뒤로가기 로직(핸드폰 뒤로가기 버튼 클릭시)
   Future<bool> onGoBack() async {
-    if (await controller.canGoBack()) { // => Webview의 뒤로가기가 가능하면
+    if (await controller.canGoBack()) {
+      // => Webview의 뒤로가기가 가능하면
       controller.goBack(); // => Webview 뒤로가기
       return Future.value(false); // => onWillPop은 false면 앱을 끄지 않는다.
     } else {
@@ -54,9 +64,14 @@ class _WebViewMainState extends State<WebViewMain> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WillPopScope(
-        child: WebViewWidget(controller: controller),
-        onWillPop: () => onGoBack(),
+      body: Stack(
+        children: [
+          WillPopScope(
+            child: WebViewWidget(controller: controller),
+            onWillPop: () => onGoBack(),
+          ),
+          const OnlyOnePointerRecognizerWidget()
+        ],
       ),
     );
   }
